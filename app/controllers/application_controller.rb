27a954361c364537d7de9a5134673ca9fa7258b5
jhassell <jedhassell@gmail.com>
2012-04-16 18:10:20 -0400

    added random search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def index
-      # create random string here for index
+    # create random string here for index
   end
 
   def create_drop
     drop_name = params[:drop_name]
     # add code here to check to see if drop exist?
+
+    drop_name = (0..5).map { rand(36).to_s(36) }.join if drop_name.empty?
 
     redirect_to "/#{drop_name}"
   end
