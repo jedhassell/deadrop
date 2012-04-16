4a0fd2044d623188c40db0d028f37c3edfe77324
jhassell <jedhassell@gmail.com>
2012-04-16 17:52:21 -0400

    changed navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   def index
       # create random string here for index
   end
+
+  def create_drop
+    drop_name = params[:drop_name]
+    # add code here to check to see if drop exist?
+
+    redirect_to controller: 'deadrop', action: drop_name
+  end
 end
